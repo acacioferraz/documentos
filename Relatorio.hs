cfd8f2cf593d5b258cee243125e79a427750230a
Acácio Ferraz <62916234+acacioferraz@users.noreply.github.com>
2020-11-17 15:09:56 -0300

    Update Relatorio.hs
    Finalização do projeto de vendas, todas as funções e realizando o que foi proposto para o projeto.

--- Relatorio.hs
+++ Relatorio.hs
@@ -1,8 +1,5 @@
 module Relatorio where
 import System.IO
-{-relatorio :: Int -> IO()
-relatorio n = putStr (cabecalho ++ corpo ++ rodape)
--}
 
 --Universidade Estadual do Sudoesta da Bahia
 --Curso Ciência da Computação
@@ -15,34 +12,37 @@ relatorio n = putStr (cabecalho ++ corpo ++ rodape)
 tamanhoLinha :: Int
 tamanhoLinha = 60
 
+--cabecalho do relatorio
 cabecalho :: String
 cabecalho = imprimir tamanhoLinha " " ++ "\n" ++ 
 			imprimirTitulo ++ "\n" ++
 			imprimir tamanhoLinha " " ++ "\n" ++
 			titulo2 ++ "\n"
 
+--imprime o titulo do arquivo
 imprimirTitulo :: String
 imprimirTitulo = imprimir quantidade " " ++ titulo ++ imprimir quantidade " " 
 	where
 		quantidade :: Int
 		quantidade = div (tamanhoLinha - (length titulo)) 2
 
+--string que contem o titulo
 titulo :: String
 titulo = "Relatorio de Vendas"
 
 titulo2 :: String
-titulo2 = "Mes         Quantidade         R$ "
+titulo2 = "Mes                        Quantidade                   R$ "
 
+--imprime uma sequencia de qualquer caracter
 imprimir :: Int -> String -> String
-imprimir 0 c = " "
-imprimir n c = c ++ imprimir(n-1) c
+imprimir 0 esp = " "
+imprimir n esp = esp ++ imprimir(n-1) esp
 
-corpo :: String 
-corpo = " "
+--imprime o corpo do projeto com os meses valores e quantidade de vendas correspondentes a cada mes
+corpo :: Int -> String 
+corpo n = imprimirLinhas n
 
---rodape :: Int -> String
---rodape n = 30 ++ "\n" ++ "Mes "++ "         "  ++ "Quantidade" ++ "\n" ++ espacamento (25 - length(imprimirLinhas n)) ++ imprimirLinhas n
-
+--funcao que retorna a quantidade de vendas correspondentes ao mes
 vendasMes :: Int -> Int
 vendasMes mes 
 			|mes == 1 = 20
@@ -69,6 +69,7 @@ soma n
 total :: Int 
 total = soma 12
 
+-- função que calcula se houve alguns mes com zero de vendas
 vendazero :: Int -> Int
 vendazero 0 = 0
 vendazero n 
@@ -94,6 +95,7 @@ menor n
 	| n == 1 = vendasMes 1
 	| otherwise = min(vendasMes n) (menor(n-1)) 
 
+-- imprime o mes que corresponde ao numero 1 - 12
 imprimemes :: Int -> String
 imprimemes n = impmes n 
 	where
@@ -101,10 +103,7 @@ imprimemes n = impmes n
 			| n == 12 = show (mes n) ++ "\n"
 			| otherwise = show (mes n) ++ "\n" ++ impmes (n+1)
 
---emprime :: Int
---emprime = imprimemes 12
-
--- mes referente ao numero
+-- numero referente ao mes
 mes :: Int -> String
 mes n 
 	| n==1 = "Janeiro"
@@ -119,26 +118,56 @@ mes n
 	| n==10 = "Outubro"
 	| n==11 = "Novembro"
 	| n==12 = "Dezembro"
-	|otherwise = ""
+	| otherwise = ""
 
 valor :: Int
 valor = 12
 
-imprimirLinha :: Int -> String
-imprimirLinha n = mes n ++ "     " 
-				++ show (vendasMes n) 
-				++ "        R$ " 
-				++ show (valor*(vendasMes n)) ++ "\n"
+-- calcula o tamanho da string no caso o nome do mes
+tamanho :: String -> Int
+tamanho "" = 0
+tamanho (a:b) = 1 + tamanho b 
 
+--imprime a linha tabulada com nome do mes, numero de vendas e total em R$
+imprimirLinha :: Int -> String
+imprimirLinha n = mes n ++ imprimir (30 - tamanho (mes n)) " " 
+				++ show (vendasMes n) 
+				++ imprimir 20 " " ++ "R$ " 
+				++ show (valor*(vendasMes n)) ++ ",00" ++ "\n"
+
+-- imprime na forma recursiva a quantidade de meses solicitada
 imprimirLinhas :: Int -> String
 imprimirLinhas n 
 	|n == 1 = imprimirLinha 1
 	|otherwise =  imprimirLinhas(n-1) ++ imprimirLinha(n)
 
+-- imprime o rodape com valores do balanço anual tabulado
+
+rod1 :: String
+rod1 = "Total"
+
+rod2 :: String
+rod2 = "Maior"
+
+rod3 :: String
+rod3 = "Menor"
+
+rod4 :: String
+rod4 = "Venda Zerada"
+
+rod5 :: String
+rod5 = "Preço do Produto"
 
 rodape :: String
-rodape = "Total        " ++ show(total) ++ "     R$ " ++ show (total*12) ++ "\n" 
-		++ "Maior        " ++ show(maior 12) ++ "\n"
-		++ "Menor        " ++ show(menor 12) ++ "\n"
-		++ "Venda Zerada " ++ show (vendazero 12) ++ "\n"
+rodape = "\n" ++ rod1 ++ imprimir (30 - tamanho (rod1)) " " ++ show total ++ imprimir 20 " " ++ "R$ " ++ show (total*12) ++ ",00"++ "\n" 
+		++ rod2 ++ imprimir (30 - tamanho (rod2)) " "  ++ show(maior 12) ++ "\n"
+		++ rod3 ++ imprimir (30 - tamanho (rod3)) " "  ++ show(menor 12) ++ "\n"
+		++ rod4 ++ imprimir (30 - tamanho (rod4)) " "  ++ show (vendazero 12) ++ "\n" ++ "\n"
+		++ rod5 ++ imprimir (30 - tamanho (rod5)) " "  ++ show valor ++ ",00" ++ "\n\n" 
+
+--função para impressão do relatorio de vendas com cabeçalho, corpo e rodape
+relatorio :: Int -> IO()
+relatorio n = putStr ((cabecalho) ++ (corpo n) ++ (rodape))
+
+
 
